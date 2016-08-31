5f92f3a524c998d5ee3e9a1fdda86098688f8405
sam <sam>
2016-08-31 14:37:36 +0530

    navigation

--- src/app/common/navigation/navigation.controller.js
+++ src/app/common/navigation/navigation.controller.js
@@ -1,6 +1,14 @@
 class NavigationController {
   constructor() {
     this.name = 'E X I L E Trainings';
+
+    this.menuItems = [
+    	{
+    		name: 'About',
+    		component: 'about'
+    	}
+    ]
+
   }
 }
 
